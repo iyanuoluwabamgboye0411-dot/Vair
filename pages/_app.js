44c44f6983e87cee291ee88928de5511b9c9ee12
iyanuoluwabamgboye0411-dot <iyanuoluwabamgboye0411@gmail.com>
2026-03-27 06:25:40 +0100

    Create pages/_app.js file with MyApp component implementation

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import '../styles/globals.css';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.route}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+export default MyApp;
